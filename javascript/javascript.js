be93ce4266c3df55a1749c1d86db28a0b92fa15f
Laura Unaeze <l.unaeze5@gmail.com>
2017-09-02 15:45:26 -0700

    Added firebase database to project.

---
+++ javascript/javascript.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBUpPm3dU-vknCmRd6llCDFfto-e7nlTMw",
+    authDomain: "train-scheduler-bc78f.firebaseapp.com",
+    databaseURL: "https://train-scheduler-bc78f.firebaseio.com",
+    projectId: "train-scheduler-bc78f",
+    storageBucket: "train-scheduler-bc78f.appspot.com",
+    messagingSenderId: "964709272987"
+  };
+
+  firebase.initializeApp(config);
+
+  
