6b03f12b5827ccc7b6c54403f6ce7f5d02583ce4
Laura Unaeze <l.unaeze5@gmail.com>
2017-09-02 16:42:43 -0700

    Initialized variables and started submit button onclick function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -10,4 +10,28 @@
 
   firebase.initializeApp(config);
 
-  
+  //initial variables
+  var trainName = "";
+  var destination = "";
+  var firstTrainTime = 0; //time. not sure in what format yet
+  var frequency = 0; //time in minutes
+
+
+  //variable that references firebase.database
+  var database = firebase.database();
+
+
+  $("#submit-button").on("click", function(event){
+  	//prevents form from submitting before all info is entered in
+  	event.preventDefault();
+
+
+  	
+
+
+
+
+
+
+  })
+
